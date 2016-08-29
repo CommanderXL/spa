faef04dc2bdb6c11a612096229468863d744ca28
XRene <xiaolei@didichuxing.com>
2016-08-30 07:47:59 +0800

    添加avatar和登出功能

--- js/spa.fake.js
+++ js/spa.fake.js
@@ -3,7 +3,7 @@
  */
 
 spa.fake = (function () {
-    var getPeopleList, fakeIdSerial, makeFakeId, mockSio;
+    var peopleList, getPeopleList, fakeIdSerial, makeFakeId, mockSio;
 
     //添加模拟的服务端ID序号计数器
     fakeIdSerial = 5;
@@ -13,56 +13,130 @@ spa.fake = (function () {
         return 'id_' + String(fakeIdSerial++);
     };
 
-    getPeopleList = function () {
-        return [
-            {
-                name: 'Btter',
-                _id: 'id_01',
-                css_map: {
-                    top: 20,
-                    left: 20,
-                    background: 'rgb(128, 128, 128)'
-                }
-            },
-            {
-                name: 'Mike',
-                _id: 'id_02',
-                css_map: {
-                    top: 60,
-                    left: 20,
-                    background: 'rgb(128, 255, 128)'
-                }
-            },
-            {
-                name: 'Pebbles',
-                _id: 'id_03',
-                css_map: {
-                    top: 100,
-                    left: 20,
-                    background: 'rgb(128,192, 192)'
-                }
+    peopleList = [
+        {
+            name: 'Btter',
+            _id: 'id_01',
+            css_map: {
+                top: 20,
+                left: 20,
+                background: 'rgb(128, 128, 128)'
             }
-        ]
-    };
+        },
+        {
+            name: 'Mike',
+            _id: 'id_02',
+            css_map: {
+                top: 60,
+                left: 20,
+                background: 'rgb(128, 255, 128)'
+            }
+        },
+        {
+            name: 'Pebbles',
+            _id: 'id_03',
+            css_map: {
+                top: 100,
+                left: 20,
+                background: 'rgb(128,192, 192)'
+            }
+        }
+    ];
 
     mockSio = (function () {
-        var on_sio, emit_sio, callback_map = {};
+        var on_sio, emit_sio, emit_mock_msg,
+            send_listchange, listchange_idto,
+            callback_map = {};
 
         on_sio = function (msg_type, callback) {
             callback_map[msg_type] = callback;
         };
 
         emit_sio = function (msg_type, data) {
+            var person_map, i;
+            //有新用户加入
             if(msg_type === 'adduser' && callback_map.userupdate) {
                 setTimeout(function () {
-                    callback_map.userupdate([{
-                        id: makeFakeId(),
+                    person_map = {
+                        _id: makeFakeId(),
                         name: data.name,
                         css_map: data.css_map
-                    }]);
+                    };
+                    peopleList.push(person_map);
+                    callback_map.userupdate([person_map]);
                 }, 3000);
             }
-        }
+
+            //延迟2s使用模拟的响应对发送的消息进行响应
+            if(msg_type === 'updatechat' && callback_map.updatechat) {
+                setTimeout(function () {
+                    var user = spa.model.people.get_user();
+                    callback_map.updatechat({
+                        dest_id: user.id,
+                        dest_name: user.name,
+                        sender_id: data.dest_id,
+                        msg_text: 'Thanks for the node, ' + user.name
+                    });
+                }, 2000);
+            }
+
+            //如果接收的信息为leavechat.则清除chat使用的回调函数.
+            //包括聊天信息的更新,以及人员列表的更新
+            if(msg_type === 'leavechat') {
+                delete callback_map.listchange;
+                delete callback_map.updatechat;
+
+                if(listchange_idto) {
+                    clearTimeout(listchange_idto);
+                    listchange_idto = undefined;
+                }
+                send_listchange();
+            }
+
+            if(msg_type === 'updateavatar' && callback_map.listchange) {
+                for(i = 0; i < peopleList.length; i++) {
+                    if(peopleList[i]._id === data.person_id) {
+                        peopleList[i].css_map = data.css_map;
+                        break;
+                    }
+                }
+                //updateavatar事件会触发listchange事件的发生
+                callback_map.listchange([peopleList]);
+            }
+        };
+
+
+        emit_mock_msg = function () {
+            setTimeout(function () {
+                var user = spa.model.people.get_user();
+                if(callback_map.updatechat) {
+                    callback_map.updatechat([{
+                        dest_id: user.id,
+                        dest_name: user.name,
+                        sender_id: 'id_04',
+                        msg_text: 'Hi there ' + user.name + '! Wilma here.'
+                    }]);
+                }
+                else {emit_mock_msg();}
+            }, 8000)
+        };
+
+        //try once per second to use listchange callback
+        //Stop trying after first sucess
+        send_listchange = function () {
+            listchange_idto = setTimeout(function () {
+                if(callback_map.listchange) {
+                    callback_map.listchange([peopleList]);
+                    //当用户登录后,每8s发送一条模拟数据
+                    emit_mock_msg();
+                    listchange_idto = undefined;
+                } else {
+                    send_listchange();
+                }
+            }, 1000);
+        };
+
+        send_listchange();
 
         return {
             emit: emit_sio,
@@ -71,7 +145,6 @@ spa.fake = (function () {
     })();
 
     return {
-        getPeopleList: getPeopleList,
         mockSio: mockSio
     }
 })();
--- js/spa.model.js
+++ js/spa.model.js
@@ -14,7 +14,7 @@ spa.model = (function () {
             people_cid_map: {}, //保留people_cid_map键,用来保存person对象映射,键为客户端ID(cid)
             people_db: TAFFY(),  //保留TaffyDB集合,初始化为空
             user: null,
-            is_connected: fasle //是否在聊天室
+            is_connected: false //是否在聊天室
         },
         isFakeData = true,
 
@@ -47,6 +47,9 @@ spa.model = (function () {
         stateMap.user.id = user_map._id;
         stateMap.user.css_map = user_map.css_map;
         stateMap.people_cid_map[user_map._id] = stateMap.user;
+
+        //用户完成登入后就会自动加入聊天室
+        chat.join();
 
         //when we add chat, we should join here
         //事件分发
@@ -148,11 +151,17 @@ spa.model = (function () {
 
         //登出
         logout = function () {
-            var is_removed, user = stateMap.user;
+            var user = stateMap.user;
 
-            is_removed = removePerson(user);
+            //离开聊天室
+            chat._leave();
+
+            //is_removed = removePerson(user);
             //用户登出后变成了匿名用户
             stateMap.user = stateMap.anon_user;
+
+            //在登出时清除人员集合
+            clearPeopleDb();
 
             //发布spa-logout事件
             $.gevent.publish('spa-logout', [user]);
@@ -170,13 +179,16 @@ spa.model = (function () {
     })();
 
     chat = (function () {
-        var _publish_listchange,
-            _update_list, _leave_chat, join_chat;
+        var _publish_listchange, _publish_updatechat,
+            _update_list, _leave_chat,
+            get_chatee, send_msg, set_chatee,
+            join_chat, chatee = null, update_avatar;
 
         //接收新的人员列表,刷新people对象
         _update_list = function (arg_list) {
-            var i, person_map, make_person_map,
-                people_list = arg_list[0];
+            var i, person_map, make_person_map, person,
+                people_list = arg_list[0],
+                is_chatee_online = false;
 
             //清楚people列表
             clearPeopleDb();
@@ -198,10 +210,20 @@ spa.model = (function () {
                     id: person_map.id,
                     name: person_map.name
                 };
+                person = makePerson(make_person_map);
+
+                if(chatee && chatee.id === make_person_map.id) {
+                    is_chatee_online = true;
+                    chatee = person;    //如果找到了听者,就更新为新的对象
+                }
 
                 makePerson(make_person_map);
             }
             stateMap.people_db.sort('name');
+
+            //如果chatee不在线,将chatee置空
+            //这将会触发spa-setchatee全局事件
+            if(chatee && !is_chatee_online) {set_chatee('');}
         };
 
         //携带的数据为更新后的人员列表,接收到来自后端的消息时会触发这段代码
@@ -210,6 +232,115 @@ spa.model = (function () {
 
             $.gevent.publish('spa-listchange', [arg_list]);
         };
+
+        //发布spa-updatechat事件,携带的数据是消息的详细消息的映射
+        _publish_updatechat = function (arg_list) {
+            var msg_map = arg_list[0];
+
+            if(!chatee) {set_chatee(msg_map.sender_id)}
+            else if(msg_map.sender_id !== stateMap.user.id && msg_map.sender_id !== chatee.id) {
+                set_chatee(msg_map.sender_id);
+            }
+
+            $.gevent.publish('spa-updatechat', [msg_map]);
+        };
+
+
+        //向后端发送leavechat方法,并清除当前用户状态
+        _leave_chat = function () {
+            var sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+            chatee = null;
+
+            stateMap.is_connected = false;
+
+            if(sio) sio.emit('leavechat');
+        };
+
+        //创建get_chatee方法,返回chatee人员对象
+        get_chatee = function () {
+            return chatee;
+        };
+
+        //加入list
+        join_chat = function () {
+            var sio;
+
+            if(stateMap.is_connected) return false;
+            //如果用户还是匿名的
+            if(stateMap.user.get_is_anon()) {
+                console.warn('User must be defined before joining chat');
+                return false;
+            }
+
+            sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+            //在fake中添加listchange事件及其回调
+            //当有人加入聊天室时,响应这个方法,并分发spa-listchange事件
+            sio.on('listchange', _publish_listchange);
+            //在fake中添加updatechat事件及其回调
+            //绑定_publish_updatechat函数,处理从后端接收到的updatechat消息.
+            //每当接收到消息的时候,会发布spa-updatechat事件
+            sio.on('updatechat', _publish_updatechat);
+            //更改用户状态
+            stateMap.is_connected = true;
+            return true;
+        };
+
+        //更新头像的方法
+        update_avatar = function (avatar_update_map) {
+            var sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+            if(sio) {
+                sio.emit('updateavatar', avatar_update_map);
+            }
+        };
+
+        //创建send_msg方法,发送文本消息和相关的信息消息
+        send_msg = function (msg_txt) {
+            var msg_map,
+                sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+
+            if(!sio) return false;
+            if(!(stateMap.user && chatee)) return false;
+
+            //把消息和相关的详细信息构造为映射
+            msg_map = {
+                dest_id: chatee.id,
+                dest_name: chatee.name,
+                sender_id: stateMap.user.id,
+                msg_text: msg_txt
+            };
+
+            _publish_updatechat([msg_map]);
+            sio.emit('updatechat', msg_map);
+            return true;
+        };
+
+        set_chatee = function (person_id) {
+            var new_chatee;
+            new_chatee = stateMap.people_cid_map[person_id];
+
+            if(new_chatee) {
+                if(chatee && chatee.id === new_chatee.id) {
+                    return false;
+                }
+            } else {
+                new_chatee = null;
+            }
+
+            //发布spa-setchatee事件,携带的数据是old_chatee和new_chatee
+            $.gevent.publish('spa-setchatee', {old_chatee: chatee, new_chatee: new_chatee});
+            chatee = new_chatee;
+            return true;
+        };
+
+        //向外暴露leave和Join方法
+        return {
+            _leave: _leave_chat,
+            join: join_chat,
+            get_chatee: get_chatee,
+            send_msg: send_msg,
+            set_chatee: set_chatee,
+            update_avatar: update_avatar
+        }
     })();
 
 
@@ -226,7 +357,7 @@ spa.model = (function () {
         stateMap.user = stateMap.anon_user;
 
         //从fake模块获取的数据
-        if(isFakeData) {
+        /*if(isFakeData) {
             people_list = spa.fake.getPeopleList();
             for(i = 0; i < people_list.length; i++) {
                 person_map = people_list[i];
@@ -237,12 +368,13 @@ spa.model = (function () {
                     name: person_map.name
                 });
             }
-        }
+        }*/
     };
 
 
     return {
         initModule: initModule,
-        people: people
+        people: people,
+        chat: chat
     }
 })();
